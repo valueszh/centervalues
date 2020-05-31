644426077015a93c996d9914bfc1ee72280c8f13
CenterValues <66200301+CenterValues@users.noreply.github.com>
2020-05-31 15:32:02 +0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,254 @@
+questions = [
+    {
+        "question": "Giving lots of power to megacorporations is not much different from giving same ammount of power to the goverment.",
+        "effect": {
+            "ther": 10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+        {
+        "question": "Nazis and communists are basically the same.",
+        "effect": {
+            "ther": 10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Anarcho-(insert_random_ideology_name) is the same as any other type of anarchism.",
+        "effect": {
+            "ther": 10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Totalitarian regimes = literally 1984, no matter the policies.",
+        "effect": {
+            "ther": 10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Anti-fascist and social justice movements are as biggoted as those whom they oppose.",
+        "effect": {
+            "ther": 10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Everyone far-right is a nazi.",
+        "effect": {
+            "ther": 10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Everyone far-left is a stalinist.",
+        "effect": {
+            "ther": 10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Current political status quo is okay, even if it needs minor improvements.",
+        "effect": {
+            "ther": 0,
+            "belf": -10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Whoever wrote the previous question has a major case of dumb.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "Technological, cultural and societal progress go hand-in-hand with each other.",
+        "effect": {
+            "ther": 0,
+            "belf": -10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "The traditon/progress axis is stupid. Supporting science should not be equated to being pro-lgbt or pro-racial equality. The same goes the other way around.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "Extremists and/or a group I dislike deserve to fucking die. That's the only way to achieve peace.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "Authoritarian goverment allows the ones in control to commit serious crimes against people's rights, which is bad, but that issue shouldn't be resolved by the drastic measures of removing authority altogether.",
+        "effect": {
+            "ther": 0,
+            "belf": -10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "Saying that all races/cultures are the exact same is wrong, but so is thinking that racial/cultural background is the main factor in ones actions.",
+        "effect": {
+            "ther": 0,
+            "belf": -10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "The most drastic measure for political change is a demonstration/protest. Anything more extreme than that is just meaningless violence.",
+        "effect": {
+            "ther": 0,
+            "belf": -10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+               {
+        "question": "Serious political media does not interest me.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 10,
+            "cord": 0
+        }
+    },
+                   {
+        "question": "I do not want to vote on any political candidate/party.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 10,
+            "cord": 0
+        }
+    },
+                   {
+        "question": "I know the name of the president, prime minister, chancellor and multiple other politicians of my country.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+    },
+  {
+        "question": "I have never learned history/Forgot most of it.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 10,
+            "cord": 0
+        }
+    },
+ {
+        "question": "My knowledge of politics mostly comes from internet memes and/or satirical media.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 10,
+            "cord": 0
+        }
+    },
+ {
+        "question": "I have debated about my political beliefs multiple times.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+    },
+    {
+        "question": "I agree with ONLY ONE of these: Righ-Wingers generally provide more logical and constructive arguments | Left-Wingers are reasonable.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": 10
+        }
+    },
+        {
+        "question": "I agree with ONLY ONE of these: The freer the markets, the freer the people | The freer the people, the freer the markets.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": 10
+        }
+    },
+       {
+        "question": "I agree with ONLY ONE of these: White identarians are not racists | Furries are not gay.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": 10
+        }
+     },
+            {
+        "question": "I agree with ONLY ONE of these: Communalism means robbery | Social darwinism means murder.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": 10
+        }
+     },
+          {
+        "question": "I agree with ONLY ONE of these: Totalitarism is an unnecessary evil | Anarchy is unnecessary chaos.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": 10
+        }
+     },
+        {
+        "question": "I agree with ONLY ONE of these: Most herbivore animals are opportunistic carnivores | Animal cruelty is an issue that needs to be dealt with.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": 10
+        }
+     },
+      {
+        "question": "I agree with ONLY ONE of these: Police don't need guns to protect people | Citizens don't need guns to protect themselves.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": 10
+        }
+     },
+];
