9fa5e1b658a9e93f8195b781867175da64b82327
CenterValues <66200301+CenterValues@users.noreply.github.com>
2020-05-31 17:34:23 +0300

    Create ideologies.js

---
+++ ideologies.js
@@ -0,0 +1,128 @@
+ideologies = [
+    {
+        "name": "Self-righteous Cowboy",
+        "stats": {
+            "ther": 0,
+            "belf": 50,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Number of people = Number of ideologies",
+        "stats": {
+            "ther": 100,
+            "belf": 50,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Centristic Centrism",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+        {
+        "name": "Greguevarianist",
+        "stats": {
+            "ther": 50,
+            "belf": 0,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Bullied Kid",
+        "stats": {
+            "ther": 50,
+            "belf": 100,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Top Chef",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 0,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Modern-day Politician",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 100,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Universal Center",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 50,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Enlightened Centrist",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 50,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Just-me-thnostatist",
+        "stats": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Sad Loner",
+        "stats": {
+            "ther": 0,
+            "belf": 100,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+        {
+        "name": "Ironic Anti-Centrism",
+        "stats": {
+            "ther": 100,
+            "belf": 0,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Liberal Centrism",
+        "stats": {
+            "ther": 100,
+            "belf": 100,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Unironic Anti-Centrism",
+        "stats": {
+            "ther": 100,
+            "belf": 0,
+            "polc": 50,
+            "cord": 0
+        }
+    },
+];
