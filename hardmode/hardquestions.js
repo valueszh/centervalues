53dc11b38cf93e5d4fc69306d8c3bb4d6b26aa28
CenterValues <66200301+CenterValues@users.noreply.github.com>
2020-06-01 13:41:11 +0300

    Create hardquestions.js

---
+++ hardmode/hardquestions.js
@@ -0,0 +1,254 @@
+questions = [
+    {
+        "question": "Giving lots of power to megacorporations is not much different from giving same ammount of power to the goverment.",
+        "effect": {
+            "ther": -10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+        {
+        "question": "Nazis and communists are basically the same.",
+        "effect": {
+            "ther": -10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+            {
+        "question": "Anarcho-(insert_random_ideology_name) is the same as any other type of anarchism.",
+        "effect": {
+            "ther": -10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+            {
+        "question": "Totalitarian regimes = literally 1984, no matter the policies.",
+        "effect": {
+            "ther": -10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+            {
+        "question": "Anti-fascist and social justice movements are as biggoted as those whom they oppose.",
+        "effect": {
+            "ther": -10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+            {
+        "question": "Everyone far-right is a nazi.",
+        "effect": {
+            "ther": -10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+            {
+        "question": "Everyone far-left is a stalinist.",
+        "effect": {
+            "ther": -10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+            {
+        "question": "All libs are leftists.",
+        "effect": {
+            "ther": -10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+            {
+        "question": "Theists and atheists are equally vocal and annoying.",
+        "effect": {
+            "ther": -10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+    {
+        "question": "I would not participate in any kind of protest.",
+        "effect": {
+            "ther": 0,
+            "polc": -10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+               {
+        "question": "Serious political media does not interest me.",
+        "effect": {
+            "ther": 0,
+            "polc": -10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+                   {
+        "question": "I do not want to vote on any political candidate/party.",
+        "effect": {
+            "ther": 0,
+            "polc": -10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+                   {
+        "question": "I know the name of the president, prime minister, chancellor and multiple other politicians of my country.",
+        "effect": {
+            "ther": 0,
+            "polc": 10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+  {
+        "question": "I have never learned history/Forgot most of it.",
+        "effect": {
+            "ther": 0,
+            "polc": -10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+ {
+        "question": "My knowledge of politics mostly comes from internet memes and/or satirical media.",
+        "effect": {
+            "ther": 0,
+            "polc": -10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+ {
+        "question": "I have debated about my political beliefs multiple times.",
+        "effect": {
+            "ther": 0,
+            "polc": 10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+    {
+        "question": "Whenever someone talks about something even slightly related to social issues, I tell them to quit being so political or at least think about telling them so.",
+        "effect": {
+            "ther": 0,
+            "polc": -10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+    {
+        "question": "If I see a political show and/or news on TV, I change the channel.",
+        "effect": {
+            "ther": 0,
+            "polc": -10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+    },
+      {
+        "question": "The more axes a political test has, the better.",
+        "effect": {
+            "ther": 10,
+            "polc": 0,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+     },
+      {
+        "question": "All modern-day politicians are extremely close to each other in their political beliefs.",
+        "effect": {
+            "ther": -10,
+            "polc": -10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+     },
+      {
+        "question": "Not choosing a lesser evil will give rise to a greater evil.",
+        "effect": {
+            "ther": 0,
+            "polc": 10,
+            "wing": 0,
+            "govt": 0,
+            "vore": 0,
+            "helf": 0,
+            "smrt": 0,
+        }
+     },
+];
