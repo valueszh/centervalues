5b55b078ef0c8b0e713ecc2f48d016dd53e3a7db
valueszh <118618537+valueszh@users.noreply.github.com>
2022-12-28 14:09:37 +0800

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -576,7 +576,7 @@ ideologies = [
         }
     },
     {
-        "name": "My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.",
+        "name": "我名叫吉良吉影，33岁。住在杜王町东北部的别墅区一带，未婚。我在龟友连锁店服务。每天都要加班到晚上8点才能回家。我不抽烟，酒仅止于浅尝。晚上11点睡，每天要睡足8个小时。睡前，我一定喝一杯温牛奶，然后做20分钟的柔软操，上了床，马上熟睡。一觉到天亮，早上起来就像婴儿一样不带任何疲劳和压力迎接第二天。医生都说我没有任何异常。我在向你说明我是一直希望保持内心平静生活的人，不执着于胜负，不纠结于烦恼，不树立让我夜不能寐的敌人，这就是我对社会的态度，也知道这是我的幸福。不过就算打起来我也不会输给任何人就是了。",
         "stats": {
             "ther": 50,
             "belf": 50,
@@ -585,7 +585,7 @@ ideologies = [
         }
     },
         {
-        "name": "Political Cartographer",
+        "name": "政治制图师",
         "stats": {
             "ther": 30,
             "belf": 40,
@@ -594,7 +594,7 @@ ideologies = [
         }
     },
     {
-        "name": "Neo-Metacentrism",
+        "name": "新元中立主义",
         "stats": {
             "ther": 40,
             "belf": 40,
